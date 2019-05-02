0611097c3758a7192cf0b1b6a7dc2a247a4fe967
jonathanccardoso <jonathan99moura@hotmail.com>
2019-05-02 14:41:27 -0300

    Update classes

--- Mercadinho-J-J/src/Mercado/MercadoBean.java
+++ Mercadinho-J-J/src/Mercado/MercadoBean.java
@@ -1,5 +1,65 @@
 package Mercado;
 
-public class MercadoBean {
+import Cliente.ClienteBean;
+import Venda.VendaBean;
 
+public class MercadoBean {
+    private String nome;
+    private String proprietario;
+    private int CNPJ;
+    private ClienteBean[] clientes;
+    private VendaBean[] vendas;
+
+    public MercadoBean() {
+        super();
+    }
+
+    public MercadoBean(String nome, String proprietario, int cNPJ, ClienteBean[] clientes, VendaBean[] vendas) {
+        super();
+        this.nome = nome;
+        this.proprietario = proprietario;
+        CNPJ = cNPJ;
+        this.clientes = clientes;
+        this.vendas = vendas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(String proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    public int getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(int cNPJ) {
+        CNPJ = cNPJ;
+    }
+
+    public ClienteBean[] getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(ClienteBean[] clientes) {
+        this.clientes = clientes;
+    }
+
+    public VendaBean[] getVendas() {
+        return vendas;
+    }
+
+    public void setVendas(VendaBean[] vendas) {
+        this.vendas = vendas;
+    }
 }
